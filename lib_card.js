700407cc86648f86a5036cde82638bbf5a91efd4
Abdullah <90257926+Anduse23@users.noreply.github.com>
2021-12-12 22:31:15 -0500

    Add files via upload

--- lib_card.js
+++ lib_card.js
@@ -1,7 +1,15 @@
 "use strict";
 
-var gamesApp = gamesApp || {};
+var myapp = myapp || {};
 
-gamesApp.Card = function (a) {
+myapp.Card=function(a) {
+    a=$(a);
+    this.img=$(a.find("img")[0]);
+    this.aId=a.attr("id");
+    };
+
+    myapp.Card.prototype.isBlankOrRevealed=function() {
+
+        return this.aId===imageToCompare.aId;
+    };
     
-}
